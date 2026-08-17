81e96bb819c70ff86cdfe2a5538c86c319c79fa0
Anupam-Hari <anupam.harigopan@gmail.com>
2026-08-17 11:05:39 +0530

    fixed python parse file

--- transpiler/parser/python_parser.py
+++ transpiler/parser/python_parser.py
@@ -1,24 +1,16 @@
+import ast
 from pathlib import Path
 
-from Cython.Compiler.Main import Context
-from Cython.Compiler.Main import CompilationOptions
-from Cython.Compiler.TreeFragment import parse_from_strings
 
-
-def parse_cython_file(path: str | Path):
+def parse_python_file(path: str | Path) -> ast.Module:
 
     path = Path(path)
 
     source = path.read_text()
 
-    context = Context.from_options(
-        CompilationOptions()
-    )
-
-    tree = parse_from_strings(
-        name=str(path),
-        code=source,
-        context=context,
+    tree = ast.parse(
+        source,
+        filename=str(path),
     )
 
     return tree
